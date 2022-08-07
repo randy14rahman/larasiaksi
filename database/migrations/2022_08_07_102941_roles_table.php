fab1a20c41b1ea24220f5e7175b5f55341e2be7a
Sam <sam@Sams-MacBook-Pro.local>
2022-08-07 17:43:24 +0700

    migration test

---
+++ database/migrations/2022_08_07_102941_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->smallInteger('level')->length(2)->unsigned();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('roles');
+    }
+};
