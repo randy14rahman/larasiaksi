c686a593927bb7611f04c2ea343901fdd8d08dd8
Randy Rahman <randy14rahman@gmail.com>
2022-08-21 22:35:04 +0700

    update home dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,17 +26,31 @@ class HomeController extends Controller
     public function index()
     {
 
+        $params = [];
+        $sWhere = "";
+        if (in_array(auth()->user()->role_id, [2,3,4,5,6,7])) {
+            $params = [
+                'created_by' => auth()->user()->id,
+                'pemaraf1' => auth()->user()->id,
+                'pemaraf2' => auth()->user()->id,
+                'pettd' => auth()->user()->id,
+            ];
+            $sWhere .= " and (created_by=:created_by or pemaraf1=:pemaraf1 or pemaraf2=:pemaraf2 or pettd=:pettd) ";
+        }
+
         $sk_stats = app('db')->connection()->selectOne("SELECT
             sum(case when is_ttd is null then 1 else 0 end) proses,
             sum(case when is_ttd=1 then 1 else 0 end) as arsip,
             sum(case when is_paraf1 is null or (pemaraf2 is not null and is_paraf2 is null) then 1 else 0 end) draft,
             sum(case when (is_paraf1=1 or (pemaraf2 is not null and is_paraf2=1)) and is_ttd is null  then 1 else 0 end) paraf
         from
-            surat_keluar");
+            surat_keluar where 1=1{$sWhere}", $params);
         // Debug::dump($sk_stats);die;
 
-        $tmp_sk_trendline = app('db')->connection()->select("select tanggal_surat, count(*) as jumlah from surat_keluar sk
-        group by tanggal_surat order by tanggal_surat");
+        $tmp_sk_trendline = app('db')->connection()->select("SELECT tanggal_surat, count(*) as jumlah 
+        from surat_keluar sk
+        where 1=1{$sWhere}
+        group by tanggal_surat order by tanggal_surat", $params);
         // Debug::dump($sk_trendline);die;
 
         $sk_trendline = [];
